be0ec393cfe1125dbd2239f20d3ba36b3f0166bd
Danny Nunez <dnunez22@gmail.com>
2023-02-12 21:45:11 -0500

    bannerupdate

--- src/modules/HomeBanner/HomeBanner.tsx
+++ src/modules/HomeBanner/HomeBanner.tsx
@@ -1,4 +1,5 @@
 import { Navigation } from "swiper";
+import { Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { IBanner } from "types";
 import styles from "./homeBanner.module.scss";
@@ -6,47 +7,48 @@ import WrapLink from "components/WrapLink";
 import { PATH } from "constants/path";
 import { Image } from "components/Image";
 import { IMAGE_SIZE, resizeImageLoklok } from "constants/global";
+import React, { useState, useEffect } from "react";
 
 
 interface HomeBannerProps {
   banners: IBanner[];
 }
 
-
-
-// useEffect(() => {
-//   if (!data) return;
-//   let historyLS: IHistoryView[] = JSON.parse(localStorage.getItem(LocalStorage.history) || "[]");
-//   if (historyLS.length >= 30) {
-//     historyLS = historyLS.slice(0, 30);
-//   }
-
 const stylesSwiper = {
   borderRadius: "8px",
   overflow: "hidden",
-  backgroundColor: "#1a1a1a"
+  backgroundColor: "#000"
 };
 
 const HomeBanner = ({ banners }: HomeBannerProps) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=80d38ce4b783b1c72330ca00da8dd2d3&language=en-US&page=1')
+      .then(res => res.json())
+      .then(data => setMovies(data.results))
+  }, []);
+
   return (
     <section>
      
-      <Swiper loop navigation={true} modules={[Navigation]} style={stylesSwiper}>
-        {banners.map((banner) => (
-          <SwiperSlide key={banner.id}>
-            <WrapLink href={``}>
-              <Image
-                alt={banner.title}
-                className={styles.banner}
-                src={resizeImageLoklok(
-                  banner.imageUrl,
-                  IMAGE_SIZE.banner.width,
-                  IMAGE_SIZE.banner.height
-                )}
-              />
-            </WrapLink>
+      <Swiper loop navigation={true} autoplay={true} modules={[Navigation, Autoplay]}  style={stylesSwiper} autoplay={{delay:7000, pauseOnMouseEnter:true, disableOnInteraction:false }} speed={1000}>
+      
+      {movies.map((movie) => (
+          <SwiperSlide key={movie.id}>
+            
+            
+            <div className={styles.movietitle}>{movie.title}<div className={styles.movierating}>{movie.vote_average}</div><div className={styles.movieoverview}>{movie.overview}</div></div>
+            
+               <img src={`https://www.themoviedb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} />
+              
+
+           
           </SwiperSlide>
         ))}
+
+ 
+
       </Swiper>
     </section>
   );
