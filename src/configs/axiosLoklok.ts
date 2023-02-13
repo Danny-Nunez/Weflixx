58f2b877bf16103624a768f9e990c504b2a4f604
Danny Nunez <dnunez22@gmail.com>
2023-02-13 10:17:08 -0500

    loadingfix

--- src/configs/axiosLoklok.ts
+++ src/configs/axiosLoklok.ts
@@ -1,15 +1,20 @@
 import axios from "axios";
 import { randomDeviceId } from "utils/helper";
 
+const baseHeaders = {
+  lang: "en",
+  versioncode: "32",
+  clienttype: "android_tem3",
+  "Content-Type": "application/json",
+  "user-agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36 OPR/84.0.4316.52"
+};
+
 const axiosLoklok = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL_API,
   headers: {
-    lang: "en",
-    versioncode: "32",
-    clienttype: "android_tem3",
-    deviceid: randomDeviceId(16),
-    "user-agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36 OPR/84.0.4316.52"
+    ...baseHeaders,
+    deviceid: randomDeviceId(16)
   }
 });
 
@@ -18,13 +23,8 @@ axiosLoklok.interceptors.request.use(
     const customConfig = {
       ...config,
       headers: {
-        lang: "en",
-        versioncode: "32",
-        clienttype: "android_tem3",
-        deviceid: randomDeviceId(16),
-        sign: randomDeviceId(32),
-        "user-agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36 OPR/84.0.4316.52"
+        ...baseHeaders,
+        deviceid: randomDeviceId(16)
       }
     };
     return customConfig;
@@ -41,7 +41,7 @@ axiosLoklok.interceptors.response.use(
   },
   async (error) => {
     const { response } = error;
-    const errorResult = { ...response.data, status: response.status };
+    const errorResult = { ...response?.data, status: response?.status };
     return Promise.reject(errorResult);
   }
 );
