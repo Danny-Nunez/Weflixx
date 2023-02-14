de6f9e414251c26be89a1be5980a917acf02dea8
Danny Nunez <dnunez22@gmail.com>
2023-02-13 19:19:54 -0500

    responsive1

--- src/modules/HomeBanner/HomeBanner.tsx
+++ src/modules/HomeBanner/HomeBanner.tsx
@@ -8,26 +8,39 @@ import { PATH } from "constants/path";
 import { Image } from "components/Image";
 import { IMAGE_SIZE, resizeImageLoklok } from "constants/global";
 import React, { useState, useEffect } from "react";
+import { IoCloseOutline } from "react-icons/io5";
+import { BiLoaderAlt } from "react-icons/bi"
 
 
 interface HomeBannerProps {
   banners: IBanner[];
 }
 
+
 const stylesSwiper = {
   borderRadius: "8px",
-  overflow: "hidden",
+  
   backgroundColor: "#000"
 };
 
 const HomeBanner = ({ banners }: HomeBannerProps) => {
   const [movies, setMovies] = useState([] as any[]);
-
   useEffect(() => {
     fetch('https://api.themoviedb.org/3/movie/popular?api_key=80d38ce4b783b1c72330ca00da8dd2d3&language=en-US&page=1')
       .then(res => res.json())
       .then(data => setMovies(data.results))
   }, []);
+
+  const [modal, setModal] = useState(false);
+  const [videoLoading, setVideoLoading] = useState(true);
+
+  const openModal = () => {
+    setModal(!modal);
+  };
+
+  const spinner = () => {
+    setVideoLoading(!videoLoading);
+  };
 
   return (
     <section>
@@ -37,12 +50,62 @@ const HomeBanner = ({ banners }: HomeBannerProps) => {
       {movies.map((movie) => (
           <SwiperSlide key={movie} >
             
+            <div className={styles.movieoverlay}>
+            <div className={styles.movietitle}>{movie.title}
+            <div className={styles.movierating}>{movie.vote_average}<span className={styles.moviespan}>&#9733;</span></div>
+            <div className={styles.movieoverview}>{movie.overview}</div>
+            <WrapLink href={`/search?keyword=${movie.title}`}>
+            <button className={styles.moviewatchbutton}>WATCH NOW</button>
+            </WrapLink>
             
-            <div className={styles.movietitle}>{movie.title}<div className={styles.movierating}>{movie.vote_average}</div><div className={styles.movieoverview}>{movie.overview}</div></div>
+            
+            {/* <button onClick={openModal} className={styles.movietrailerbutton}>
+        TRAILER
+        {modal ? (
+          <section className={styles.modal__bg}>
+            <div className={styles.modal__align}>
+              <div className={styles.modal__content} modal={modal}>
+                <IoCloseOutline
+                  className={styles.modal__close}
+                  arial-label="Close modal"
+                  onClick={setModal}
+                />
+                <div className={styles.modal__video_align}>
+                  {videoLoading ? (
+                    <div className={styles.modal__spinner}>
+                      <BiLoaderAlt
+                        className={styles.modal__spinner_style}
+                        fadeIn="none"
+                      />
+                    </div>
+                  ) : null}
+                  <iframe
+                    className={styles.modal__video_style}
+                    onLoad={spinner}
+                    loading="lazy"
+                    width="800"
+                    height="500"
+                    src={`https://autoembed.to/trailer/movie/${movie.id}`}
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen>
+                    </iframe>
+                </div>
+              </div>
+            </div>
+          </section>
+        ) : null}
+      </button> */}
+
+
+            
+            </div>
             
                <Image src={`https://www.themoviedb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} />
               
-
+               
+               </div>
            
         </SwiperSlide>
         ))}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,33 +11,12 @@ import queryString from "query-string";
 import { useCallback } from "react";
 import useSWRInfinite from "swr/infinite";
 import { IBanner, ICategoryResult, IHomeSection } from "types";
-// import SimpleImageSlider from "react-simple-image-slider";
 import styles from "../styles/swipershell.module.scss";
 import React from 'react';
-import { Slide } from 'react-slideshow-image';
 
 interface HomePageProps {
   banners: IBanner[];
 }
-
-const Example = () => {
-  const slideImages = [
-    "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-    "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-    "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-  ];
-}
-
-
-
-
-// const slideImages = [
-//   { url: "https://www.themoviedb.org/t/p/original/w8Oqg1C7t5rUU9S1tN1QHi7SLc5.jpg" },
-//   { url: "https://www.themoviedb.org/t/p/original/bty0TwJGsxMqYRptgyzn28Cxq5y.jpg" },
-//   { url: "https://www.themoviedb.org/t/p/original/s16H6tpK2utvwDtzZ8Qy4qm5Emw.jpg" },
-//   { url: "https://www.themoviedb.org/t/p/original/lQsnGnrCGdjk4JcMI61qfOOiOzP.jpg" },
-//   { url: "https://www.themoviedb.org/t/p/original/jy82OESqwC1dtT406IHtRUbkrRZ.jpg" },
-// ];
 
 const HomePage = ({ banners }: HomePageProps) => {
   const getApiUrl = (index: number, prevData: ICategoryResult[] | null) => {
@@ -67,43 +46,9 @@ const HomePage = ({ banners }: HomePageProps) => {
   return (
     <LayoutPrimary>
       <div className="container">
-        
-      
-      
-  
-      
-      {/* <Slide>
-            <div className={styles.swipercontainer}>
-                <div style={{ 'backgroundImage': `url(${slideImages[0]})` }}>
-                    <span>Slide 1</span>
-                </div>
-            </div>
-            <div className={styles.swipercontainer}>
-                <div style={{ 'backgroundImage': `url(${slideImages[1]})` }}>
-                    <span>Slide 2</span>
-                </div>
-            </div>
-            <div className={styles.swipercontainer}>
-                <div style={{ 'backgroundImage': `url(${slideImages[2]})` }}>
-                    <span>Slide 3</span>
-                </div>
-            </div>
-        </Slide> */}
-      
-  
-      
-      {/* <SimpleImageSlider
-        width={900}
-        height={504}
-        images={images}
-        showBullets={true}
-        showNavs={true}
-        autoPlay={true}
-      />
-     */}
+      <div className={styles.homebannercontainer}>
         <HomeBanner banners={banners} />
-        
-        
+        </div>
         <div style={{ marginTop: "40px" }}>
           {(movies.length as number) > 0 ? (
             <MovieList>
