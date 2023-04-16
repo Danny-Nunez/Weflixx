146d60716ed29ff07f95922d7a2b10d1f6578e42
Danny Nunez <dnunez22@gmail.com>
2023-04-15 22:54:49 -0400

    api updates

--- src/modules/MediaPlayer/MediaPlayer.tsx
+++ src/modules/MediaPlayer/MediaPlayer.tsx
@@ -22,8 +22,7 @@ const MediaPlayer = ({
 }: MediaPlayerProps) => {
   const qualitiesProxy = qualities.map((q) => ({
     quality: q.quality,
-    // url: `${process.env.NEXT_PUBLIC_PROXY_LOKLOK}/m3u8?url=${encodeURIComponent(q.url)}`
-    url: 'https://ali-source.loklok.tv/597bc81948884d2f8da0277cfbcde0fb/07f8365b5c0d4606a58c2afb62e7153e-ab0820ae0a6908bc05eba2ecdf45dd6d-sd-00006.ts'
+    url: `${process.env.NEXT_PUBLIC_PROXY_LOKLOK}/m3u8?url=${encodeURIComponent(q.url)}`
   }));
   return (
     <Player src={qualitiesProxy} subtitles={subtitles} playerRef={playerRef}>
