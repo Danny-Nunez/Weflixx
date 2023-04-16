1c280424717a6c75187f3ee2c683b02c06cc0d84
Danny Nunez <dnunez22@gmail.com>
2023-04-15 22:03:19 -0400

    api updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ import "styles/reset.scss";
 import "swiper/css";
 import "swiper/css/navigation";
 import "nprogress/nprogress.css";
-import GoogleAnalytics from "components/GoogleAnalytics";
+// import GoogleAnalytics from "components/GoogleAnalytics";
 
 Modal.setAppElement("#__next");
 Modal.defaultStyles = {
@@ -39,7 +39,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <ErrorBoundary>
       <Provider store={store}>
         <Meta />
-        <GoogleAnalytics />
+        {/* <GoogleAnalytics /> */}
         <Authentication>
           <Component {...pageProps} />
           <Toaster position="top-right" />
